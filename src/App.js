a36cdb1bd7fdcd97f8212d0b0a7a9161e562cea5
mljubic <mljubic@itekako.com>
2018-05-04 18:40:47 +0200

    remove unnecessary imports from app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
 
 import Navbar from './components/Navbar/Navbar';
 import Router from './components/Router/Router';
-import { getOffices } from './actions/offices';
 
 class App extends Component {
   render() {
