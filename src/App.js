9a09df1ebb07a4421a03cb21d06953baa96e32d5
mljubic <mljubic@itekako.com>
2018-05-04 14:27:11 +0200

    add dummy map view

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import TextView from './views/TextView/TextView';
+import MapView from './views/MapView/MapView';
 
 class App extends Component {
-  render() {    
+  render() {
     return (
       <div>
         <Navbar />
         <Switch>
           <Redirect exact from='/' to='/list' />
+          <Route path='/map' component={MapView}/>
           <Route path='/:direction' component={TextView}/>
         </Switch>
       </div>
---
+++ src/views/MapView/MapView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MapView() {
+  return <div>Map</div>;
+}
+
+export default MapView;
