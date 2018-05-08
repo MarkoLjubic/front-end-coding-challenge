57215473a29ef001351bf37724937bf8709e9b4c
mljubic <mljubic@itekako.com>
2018-05-08 11:23:59 +0200

    add test for action

---
+++ src/actions/offices.test.js
@@ -0,0 +1,17 @@
+import * as actions from './offices';
+
+describe('offices', () => {
+
+  it('should generate the action correctly', () => {
+    const action = {
+      type: 'API',
+      payload: {
+        url: 'https://itk-exam-api.herokuapp.com/api/offices',
+        method: 'get',
+        success: 'SET_OFFICES'
+      }
+    };
+
+    expect(actions.getOffices()).toEqual(action);
+  });
+});
